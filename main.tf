9f7da9e530487728d036487ee9c4db46d1349d7c
Diógenes Ribeiro de Oliveira <91908375+diogenesOliver@users.noreply.github.com>
2023-05-18 17:11:58 +0000

    style: Fixing  identation

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ provider "aws" {
 }
 
 locals{
-    name           =    "ec2_instance_iac"
+    name = "ec2_instance_iac"
 }
 
 module "ec2_instance" {
-    source                 =     "terraform-aws-modules/ec2-instance/aws"
+    source = "terraform-aws-modules/ec2-instance/aws"
 
-    name                   =     local.name
+    name = local.name
 
-    instance_type          =     "t2.micro"
-    key_name               =     aws_key_pair.key_pair_ec2_instance.key_name
-    vpc_security_group_ids =     [aws_security_group.sg_to_ec2_instance.id]
-    subnet_id              =     "subnet-04627c89fe3034bbf" 
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.key_pair_ec2_instance.key_name
+    vpc_security_group_ids = [aws_security_group.sg_to_ec2_instance.id]
+    subnet_id = "subnet-04627c89fe3034bbf" 
 
     tags = {
         Name = "${local.name}.ec2.diogenes@gmail.com"
