844e6bfe5280a2d6004b21a6f2ee8e82fa296e2b
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-07-12 16:41:06 -0300

    fix: Fixing tag name

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,18 +3,18 @@ provider "aws" {
 
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
-
+  
   default_tags {
     tags = {
       Name         = local.name
-      Environmente = local.environmente
+      Environment = local.environment
     }
   }
 }
 
 locals {
   name         = "ec2_instance_iac"
-  environmente = "dev"
+  environment = "dev"
   networks = {
     dev = {
       subnet = "subnet-04627c89fe3034bbf"
@@ -30,7 +30,7 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.key_pair_ec2_instance.key_name
   vpc_security_group_ids = [aws_security_group.sg_to_ec2_instance.id]
-  subnet_id              = local.networks[local.environmente].subnet
+  subnet_id              = local.networks[local.environment].subnet
 
   tags = {
     Name = "${local.name}.ec2.diogenes@gmail.com"
